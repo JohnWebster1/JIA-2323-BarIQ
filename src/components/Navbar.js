4bf2313352f6364e803708a5f5a0c33a7d5200a2
domfernando319 <domfernando319@gmail.com>
2022-12-01 16:35:19 -0500

    Fixed purple margin spacing.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import "./Navbar.css"
 export default function Navbar() {
     const navbarStyles = {
         color: "#091133", 
@@ -7,25 +7,26 @@ export default function Navbar() {
         margin: '0px 0px',
         padding: '13px 40px',
         fontFamily: 'Roboto',
-        alignContent: "center"
+        alignContent: "center",
+        justifyContent: "space-evenly"
     }
     const navPagesStyles = {
         display: 'flex',
         margin: "0 20px",
         color: "#091133",
-        padding: "10px 10px"
+        padding: "10px 10px",
+        font: "Roboto"
     }
     const buttonStyles = {
         background: "#091133",
         borderRadius: "10px",
         height: '50px',
         width: '100px',
-        color: '#FFFFFF',
-        fontFamily: "Roboto"
+        color: '#FFFFFF'
 
     }
     return (
-        <div>
+        <div className='nav-container'>
             <nav className="navbar" style={navbarStyles}>
                 <h2 style={navbarStyles}>Bar IQ</h2>
                 <nav style={navbarStyles} >
