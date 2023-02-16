af92018b03b3fbc0872c759dad2c40dd2b34ef76
nhulston <nicholashulston@gmail.com>
2023-02-16 18:25:35 -0500

    Add articles that read from the backend. There is an article preview page with clickable previews of each article.
    When clicking an article preview, a new page is opened with the full article.

---
+++ bariq-frontend/pages/help/[id].js
@@ -0,0 +1,58 @@
+import {initFirebase} from "../../util/firebaseApp";
+import {getAuth} from "firebase/auth";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {getArticleById} from "../../util/getArticles";
+import Head from "next/head";
+import Navbar from "../../components/Navbar";
+import Centered from "../../components/Centered";
+import {useEffect, useState} from "react";
+import { useRouter } from 'next/router';
+import Article from "../../components/Article";
+
+export default function Index() {
+    // Authentication
+    initFirebase()
+    const auth = getAuth()
+    const [user, loading] = useAuthState(auth);
+
+    // Get article
+    const router = useRouter();
+    const {id} = router.query;
+    const [articleComponent, setArticleComponent] = useState(<>Loading...</>);
+    useEffect(() => {
+        if (id !== undefined) {
+            getArticleById(id).then(data => {
+                if (data === undefined) {
+                    setArticleComponent(<h1>Article not found.</h1>);
+                } else {
+                    setArticleComponent(<Article data={data}/>);
+                }
+            });
+        }
+    }, [id]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    const logout = () => {
+        auth.signOut().then(() => {
+            if (!user) {
+                router.push("/")
+            }
+        });
+    }
+
+    return (<div>
+        <Head>
+            <title>Bar IQ | Help</title>
+            <meta name="description" content="The help page for Bar IQ."/>
+            <link rel="icon" href="../public/favicon.ico"/>
+        </Head>
+
+        <Navbar loggedIn={user} logout={logout}/>
+        <Centered>
+            {articleComponent}
+        </Centered>
+    </div>)
+}
