97ba21c011a13adb9f689422be3ee23bb5bb9303
nhulston <nicholashulston@gmail.com>
2023-02-20 18:21:37 -0500

    Cleanup footer, fix resizing issues

---
+++ bariq-frontend/components/Footer.js
@@ -0,0 +1,138 @@
+import Image from "next/image";
+import styled from "styled-components";
+
+const FooterWrapper = styled.footer`
+  background: #C2C8CF;
+  width: 100%;
+`;
+
+const ImageWrapper = styled.div`
+  position: relative;
+  width: 105px;
+  height: 100px;
+  margin: -20px 0;
+`;
+
+const LinkRows = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 40px 120px 0;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const SectionTitle = styled.h2`
+  font-weight: bold;
+  font-size: 1.2rem;
+`;
+
+const Link = styled.a`
+  font-size: 1.2rem;
+  cursor: pointer;
+  display: inline-block;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const BottomRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0 120px 50px;
+  margin-top: 40px;
+`;
+
+const IconRow = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+function Footer() {
+    return <FooterWrapper>
+        <LinkRows>
+            <ImageWrapper>
+                <a href="/">
+                    <Image
+                        src={"/../public/logo.png"}
+                        alt="hi"
+                        layout="fill"
+                        objectFit="contain"
+                    />
+                </a>
+            </ImageWrapper>
+            <Column>
+                <SectionTitle>SOLUTIONS</SectionTitle>
+                <Link>Link 1</Link>
+                <Link>Link 2</Link>
+                <Link>Link 3</Link>
+                <Link>Link 4</Link>
+            </Column>
+            <Column>
+                <SectionTitle>RESOURCES</SectionTitle>
+                <Link>Link 1</Link>
+                <Link>Link 2</Link>
+                <Link>Link 3</Link>
+                <Link>Link 4</Link>
+            </Column>
+            <Column>
+                <SectionTitle>COMPANY</SectionTitle>
+                <Link>Link 1</Link>
+                <Link>Link 2</Link>
+                <Link>Link 3</Link>
+                <Link>Link 4</Link>
+            </Column>
+            <Column>
+                <SectionTitle>GET STARTED</SectionTitle>
+                <Link>Link 1</Link>
+                <Link>Link 2</Link>
+                <Link>Link 3</Link>
+                <Link>Link 4</Link>
+            </Column>
+        </LinkRows>
+
+        <BottomRow>
+            <p>© 2023 Bar IQ, LLC</p>
+            <IconRow>
+                <a className="text-gray-500">
+                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                         className="w-5 h-5" viewBox="0 0 24 24">
+                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"/>
+                    </svg>
+                </a>
+                <a className="ml-3 text-gray-500">
+                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                         className="w-5 h-5" viewBox="0 0 24 24">
+                        <path
+                            d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"/>
+                    </svg>
+                </a>
+                <a className="ml-3 text-gray-500">
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round"
+                         strokeLinejoin="round" strokeWidth="2" className="w-5 h-5"
+                         viewBox="0 0 24 24">
+                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5"/>
+                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"/>
+                    </svg>
+                </a>
+                <a className="ml-3 text-gray-500">
+                    <svg fill="currentColor" stroke="currentColor" strokeLinecap="round"
+                         strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
+                        <path stroke="none"
+                              d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"/>
+                        <circle cx="4" cy="4" r="2" stroke="none"/>
+                    </svg>
+                </a>
+            </IconRow>
+        </BottomRow>
+    </FooterWrapper>
+}
+
+export default Footer;
--- bariq-frontend/components/Sidebar/Layout.jsx
+++ bariq-frontend/components/Sidebar/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Sidebar from './Sidebar'
-import Home  from "../Home.js"
+import Home from "../Home.js"
 import Navbar from '../Navbar'
 import styled from 'styled-components'
 
@@ -10,30 +10,35 @@ const Container = styled.div`
   width: 100%;
 `;
 const Title = styled.h1`
-background: #E7EDF2;
-text-align: center;
-font-size: 20px;
-height: 60px;
-width: 100%;
-padding-top: 20px;
-padding-bottom: 20px;
+  background: #E7EDF2;
+  text-align: center;
+  font-size: 20px;
+  height: 60px;
+  width: 100%;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
 
 const Layout = (props) => {
     return (
-        <div className='h-screen flex flex-row justify-start'>
-            <Sidebar />
+        <Wrapper>
+            <Sidebar/>
             <Container>
                 <Navbar loggedIn={props.user} logout={props.logout}/>
                 <Title>Interactive Resource Portal</Title>
-                {props.loggedIn ? <Title>Welcome {props.loggedIn.displayName}! </Title>: <></>}
+                {props.loggedIn ? <Title>Welcome {props.loggedIn.displayName}! </Title> : <></>}
                 <div className='bg-secondary flex-1 p-4 text-black'>
-                    
+
                     <Home/>
-                    
+
                 </div>
             </Container>
-        </div>
+        </Wrapper>
     )
 }
 
