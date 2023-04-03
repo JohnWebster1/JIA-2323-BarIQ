3daa5a0cfd730290b5761cb1595db51bf52b9918
dannylee <dlee807@gatech.edu>
2023-04-03 15:58:19 -0400

    Video icon UI change

--- bariq-frontend/components/sidebar-icons/VideosIcon.jsx
+++ bariq-frontend/components/sidebar-icons/VideosIcon.jsx
@@ -12,10 +12,10 @@ function VideosIcon({ fill = "#white", ...rest }) {
       xmlns="http://www.w3.org/2000/svg"
       {...rest}
     >
-      <path d="M4 8H2v12a2 2 0 002 2h12v-2H4V8z" fill={"none"} />
+      <path d="M4 8H2v12a2 2 0 002 2h12v-2H4V8z" fill={"fill"} />
       <path
         d="M20 2H8a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V4a2 2 0 00-2-2zm-9 12V6l7 4-7 4z"
-        fill={"none"}
+        fill={"fill"}
       />
     </svg>
   );
